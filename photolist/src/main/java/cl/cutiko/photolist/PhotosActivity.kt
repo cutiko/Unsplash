42597a058d7b69c31b9da4428b1149dc039be808
cutiko <cutiko@gmail.com>
2019-01-02 15:33:00 -0300

    first commit: basic project structure

---
+++ photolist/src/main/java/cl/cutiko/photolist/PhotosActivity.kt
@@ -0,0 +1,12 @@
+package cl.cutiko.photolist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PhotosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photos)
+    }
+}
