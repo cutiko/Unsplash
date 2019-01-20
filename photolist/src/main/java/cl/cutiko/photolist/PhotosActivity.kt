b95ed5a024c1d4175048c4ce2bf244ed1fa75f86
cutiko <cutiko@gmail.com>
2019-01-20 20:29:25 -0300

    ADD: reminder

--- photolist/src/main/java/cl/cutiko/photolist/PhotosActivity.kt
+++ photolist/src/main/java/cl/cutiko/photolist/PhotosActivity.kt
@@ -47,6 +47,8 @@ class PhotosActivity : AppCompatActivity(), PhotosContract.Callback {
                 val color = adapter.getItemColor(lastPosition)
                 window.statusBarColor = color
                 window.navigationBarColor = color
+                /*TODO
+                https@ //stackoverflow.com/questions/5200811/in-android-how-do-i-smoothly-fade-the-background-from-one-color-to-another-ho*/
                 photosCl.setBackgroundColor(color)
             }
         })
