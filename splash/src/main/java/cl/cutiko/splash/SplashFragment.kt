7e4048d05bc4bd65f7d1da7354c796f1f3137d39
cutiko <cutiko@gmail.com>
2019-01-19 22:50:24 -0300

    ADD: from splash to photo more elegantly

--- splash/src/main/java/cl/cutiko/splash/SplashFragment.kt
+++ splash/src/main/java/cl/cutiko/splash/SplashFragment.kt
@@ -6,12 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import cl.cutiko.viewlibrary.Directions
 
 
 class SplashFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.setOnClickListener {
+            view.findNavController().navigate(Directions().toPhotos())
+        }
     }
 
 
