9912bf57bae0e3f65502e4c6e469120f0e6db6d7
cutiko <cutiko@gmail.com>
2019-01-19 22:15:20 -0300

    ADD: basic navigation

---
+++ app/src/main/java/cl/cutiko/unsplash/HostActivity.kt
@@ -0,0 +1,12 @@
+package cl.cutiko.unsplash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HostActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_host)
+    }
+}
