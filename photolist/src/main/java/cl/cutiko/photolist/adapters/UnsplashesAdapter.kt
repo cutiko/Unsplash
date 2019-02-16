2272ca7fb5009c29c6e81b4375800903568cc7cb
cutiko <cutiko@gmail.com>
2019-02-16 18:04:19 -0300

    REMOVE: adapter is data driven

--- photolist/src/main/java/cl/cutiko/photolist/adapters/UnsplashesAdapter.kt
+++ photolist/src/main/java/cl/cutiko/photolist/adapters/UnsplashesAdapter.kt
@@ -2,22 +2,18 @@ package cl.cutiko.photolist.adapters
 
 import android.content.Context
 import android.content.res.ColorStateList
-import android.graphics.Bitmap
 import android.graphics.Color
-import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
-import androidx.palette.graphics.Palette
 import androidx.recyclerview.widget.RecyclerView
 import cl.cutiko.data.models.Unsplash
 import cl.cutiko.photolist.R
 import cl.cutiko.viewlibrary.LoadingImageView
 import com.squareup.picasso.Picasso
-import com.squareup.picasso.Target
 
 class UnsplashesAdapter(context : Context) : RecyclerView.Adapter<UnsplashesAdapter.UnsplashHolder>() {
 
@@ -36,25 +32,11 @@ class UnsplashesAdapter(context : Context) : RecyclerView.Adapter<UnsplashesAdap
         val unsplash = unsplashes[position]
         val url = unsplash.urls?.small
         holder.loadingIv.setImage(url)
-
-        Picasso.get().load(url).into(object :Target {
-            override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
-            }
-            override fun onBitmapFailed(e: Exception?, errorDrawable: Drawable?) {
-            }
-
-            override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
-                if (bitmap != null) {
-                    val palette = Palette.from(bitmap).generate()
-                    val overlayColor = palette.getDarkMutedColor(black)
-                    holder.overlay.backgroundTintList = ColorStateList.valueOf(overlayColor)
-                    val foregroundColor = palette.getDarkVibrantColor(black)
-                    holder.foreground.backgroundTintList = ColorStateList.valueOf(foregroundColor)
-                    holder.name.setTextColor(palette.getLightMutedColor(white))
-                    holder.likes.setTextColor(palette.getLightVibrantColor(white))
-                }
-            }
-        })
+        val colors = unsplash.swatch
+        holder.overlay.backgroundTintList = ColorStateList.valueOf(colors.muted)
+        holder.foreground.backgroundTintList = ColorStateList.valueOf(colors.vibrant)
+        holder.name.setTextColor(colors.title)
+        holder.likes.setTextColor(colors.body)
         val user = unsplash.user
         val profile = user?.profile_image?.medium
         Picasso.get().load(profile).centerCrop().fit().into(holder.profile)
