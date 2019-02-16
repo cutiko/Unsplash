520b291796b436ced89c2712257c6c76e30fac02
cutiko <cutiko@gmail.com>
2019-02-16 15:14:58 -0300

    REFACTOR: use cases don't need to re use the coroutine

--- domain/src/main/java/cl/cutiko/domain/cases/GetPrevious.kt
+++ domain/src/main/java/cl/cutiko/domain/cases/GetPrevious.kt
@@ -3,13 +3,11 @@ package cl.cutiko.domain.cases
 import android.app.Application
 import cl.cutiko.data.models.Unsplash
 import cl.cutiko.data.repository.UnsplashRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 class GetPrevious(application: Application) {
 
     private val repo : UnsplashRepository = UnsplashRepository(application)
 
-    suspend fun getFromDb() : List<Unsplash> = withContext(Dispatchers.IO){repo.loadAll() }
+    suspend fun getFromDb() : List<Unsplash> = repo.loadAll()
 
 }
