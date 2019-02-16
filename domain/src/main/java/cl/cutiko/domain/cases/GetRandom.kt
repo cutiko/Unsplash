f4962de8d50b076654f9b6f3fda04aa96ed3f9e8
cutiko <cutiko@gmail.com>
2019-02-16 12:29:06 -0300

    FIX: coroutine scope

--- domain/src/main/java/cl/cutiko/domain/cases/GetRandom.kt
+++ domain/src/main/java/cl/cutiko/domain/cases/GetRandom.kt
@@ -2,14 +2,13 @@ package cl.cutiko.domain.cases
 
 import android.app.Application
 import cl.cutiko.domain.network.GetUnsplashes
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class GetRandom(application: Application)  {
 
     private val request : GetUnsplashes = GetUnsplashes(application)
 
-    suspend fun start() {
-        coroutineScope{request.start()}
-    }
+    suspend fun start()= withContext(Dispatchers.IO){request.start()}
 
 }
