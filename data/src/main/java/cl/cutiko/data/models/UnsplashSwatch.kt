a9916f3e63658fa347f9f1f02b97ff52932ceef7
cutiko <cutiko@gmail.com>
2019-02-16 18:03:12 -0300

    REFACTOR: colors are ordered by relevance

--- data/src/main/java/cl/cutiko/data/models/UnsplashSwatch.kt
+++ data/src/main/java/cl/cutiko/data/models/UnsplashSwatch.kt
@@ -1,8 +1,8 @@
 package cl.cutiko.data.models
 
 data class UnsplashSwatch(
+    var muted : Int = 0,
+    var vibrant : Int = 0,
     var body : Int = 0,
-    var title : Int = 0,
-    var dark : Int = 0,
-    var vibrant : Int = 0
+    var title : Int = 0
 )
